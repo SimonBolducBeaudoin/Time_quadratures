5706756758cb143b46ea03fdf2ba911a9f92ed10
Simon Bolduc Beaudoin <Simon.Bolduc.Beaudoin@usherbrooke.ca>
2024-10-03 11:32:36 -0400

    Beautifying clang-format

--- src/python_submodule.cpp
+++ src/python_submodule.cpp
@@ -1,9 +1,7 @@
 #include "python_submodule.h"
 
-//Python Binding and Time_Quad class instances.
-PYBIND11_MODULE(time_quadratures, m)
-{
-    m.doc() = "Fast caculations of time quadratrures.\n";
-	init_TimeQuad_FFT(m);
+// Python Binding and Time_Quad class instances.
+PYBIND11_MODULE(time_quadratures, m) {
+  m.doc() = "Fast caculations of time quadratrures.\n";
+  init_TimeQuad_FFT(m);
 }
-
