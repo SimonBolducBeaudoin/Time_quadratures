c2d74565203694f0af8ba5b160d570aaf4694c1d
Simon Bolduc Beaudoin <simonbolducbeaudoin@gmail.com>
2024-10-23 14:59:33 -0400

    (Very Minor) Formatting

--- src/python_submodule.cpp
+++ src/python_submodule.cpp
@@ -2,6 +2,6 @@
 
 // Python Binding and Time_Quad class instances.
 PYBIND11_MODULE(time_quadratures, m) {
-  m.doc() = "Fast caculations of time quadratrures.\n";
-  init_TimeQuad_FFT(m);
+    m.doc() = "Fast caculations of time quadratrures.\n";
+    init_TimeQuad_FFT(m);
 }
