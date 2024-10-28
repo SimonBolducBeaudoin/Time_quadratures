5d0165bd151fe58aaad5b05a298fd2bc9efa4596
Simon Bolduc Beaudoin <Simon.Bolduc.Beaudoin@usherbrooke.ca>
2024-10-28 14:28:44 -0400

    (Update) Sync utils

--- __init__.py
+++ __init__.py
@@ -1,15 +1,8 @@
 #!/bin/env/python
 #! -*- coding: utf-8 -*-
 
-import os as _os, platform as _platform
-mingw_path = 'C:\\cygwin64\\usr\\x86_64-w64-mingw32\\sys-root\\mingw\\bin'
-if _os.name == 'nt':
-    if _platform.python_version_tuple()[0]== '2' : #python 2
-        if ( mingw_path not in _os.environ['PATH'] ) :
-            _os.environ['PATH'] = mingw_path+_os.path.pathsep+_os.environ['PATH']
-    else : # python 3      
-        _os.add_dll_directory("C:/cygwin64/usr/x86_64-w64-mingw32/sys-root/mingw/bin")
-del mingw_path
+from SBB.Pyhegel_extra.Env_setup import add_Cygwin_mingw_to_path
+add_Cygwin_mingw_to_path()
 
-__all__ = ["betas","kernels","labels","time_quadratures",]
+__all__ = ["betas","kernels","labels","time_quadratures","sync_utils"]
  
---
+++ sync_utils.py
@@ -0,0 +1,37 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+import numpy as _np
+from fractions import Fraction as _Fraction 
+from typeguard import typechecked as _typechecked
+
+@_typechecked
+def get_perdiod(f: int,sampling_rate: int = 32000000000) -> int:
+    """
+    Returns the period at which the sampling rate will be commensurate with f.
+    Example f = 12 sampling_rate=32.
+        ==> Each 8 points we do a period which correspond to 3 cycles of f.
+    """
+    return _Fraction(f,sampling_rate).denominator
+   
+@_typechecked
+def get_ordered_index(f: int,sampling_rate: int = 32000000000) -> _np.ndarray:
+    """
+    Returns the index that are ordering the times inside each cycle
+    """
+    fraction=_Fraction(f,sampling_rate)
+    Period = fraction.denominator
+    Cycle = fraction.numerator
+    t_cycle=(_np.r_[0:Period]*Cycle/Period)%(1)
+    return _np.argsort(t_cycle)
+    
+@_typechecked
+def get_cycle_time(f: int,sampling_rate: int = 32000000000) -> _np.ndarray:
+    """
+    Returns the index that are ordering the times inside each cycle
+    """
+    fraction=_Fraction(f,sampling_rate)
+    Period = fraction.denominator
+    Cycle = fraction.numerator
+    t_cycle=(_np.r_[0:Period]*Cycle/Period)%(1)
+    return t_cycle
